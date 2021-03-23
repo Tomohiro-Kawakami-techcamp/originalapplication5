418b5dfd53eb85967d996c5e073b8a2395a7d630
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-03-23 10:15:10 +0900

    コントローラー、モデル作成

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,12 +1,21 @@
 class SchedulesController < ApplicationController
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
+
   def index
     @schedules = Schedule.all
   end
 
   def new
+    @schedule = Schedule.new
   end
 
   def create
+    @schedule = Schedule.new(schedule_params)
+    if @schedule.save
+      redirect_to schedules_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -19,5 +28,14 @@ class SchedulesController < ApplicationController
   end
 
   def destroy
+  end
+
+  private 
+  def schedule_params
+    params.require(:schedule).permit(:title, :content).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @schedule = Schedule.find(params[:id])
   end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,7 @@
 class Schedule < ApplicationRecord
+  belongs_to :user
+  has_many :comments
+
+  validates :title,  presence: true
+  validates :content,  presence: true
 end
